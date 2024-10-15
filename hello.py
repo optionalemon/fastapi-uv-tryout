5a04d0d5294c3bde6ea3c55eb9a87aea451c0eb9
Xu Yi <E0772330@u.nus.edu>
2024-10-15 16:53:29 +0800

    init: create uv fastapi skeleton

---
+++ hello.py
@@ -0,0 +1,10 @@
+# uv init --app
+# uv add fastapi --extra standard # add fastapi to the project
+# python hello.py # run the project
+
+def main():
+    print("Hello from fastapi-uv-tryout!")
+
+
+if __name__ == "__main__":
+    main()
